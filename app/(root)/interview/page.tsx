a1eb5fb5b25cf6a1d68573ddcfab4ba49c433f9a
laharipriya13 <laharipriya.1303@gmail.com>
2025-07-05 19:59:19 +0530

    interview generation session done

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import Agent from '@/components/ui/Agent'
-import { getCurrentUser } from '@/lib/actions/auth.action'
-const page = async() => {
-  const user = await getCurrentUser();
-  return <>
-    <h3>Interview Generation</h3>
-    <Agent userName={user?.name} userId={user?.id} type="generate"></Agent>
-  </>
-}
+import Agent from "@/components/ui/Agent";
+import {getCurrentUser} from "@/lib/actions/auth.action";
 
-export default page;
+const Page = async () => {
+    const user = await getCurrentUser();
+
+    return (
+        <>
+            <h3>Interview Generation</h3>
+
+            <Agent userName={user?.name} userId={user?.id} type="generate" />
+        </>
+    )
+}
+export default Page
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,7 +13,7 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
         <div className="root-layout">
             <nav>
                 <Link href="/"  className="flex items-center gap-2" >
-                    <Image src="/logo.png" alt="Logo" width={38} height={32} />
+                    <Image src="/logo.png" alt="Logo" width={40} height={32} />
                     <h2 className="text-primary-100">Mockly</h2>
                 </Link>
             </nav>
