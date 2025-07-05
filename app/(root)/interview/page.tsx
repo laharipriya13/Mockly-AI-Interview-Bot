6732e7dc669062ce4280d780a84792b682b14b18
laharipriya13 <laharipriya.1303@gmail.com>
2025-07-05 16:52:33 +0530

    show generated interviews on home page

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Agent from '@/components/ui/Agent'
-const page = () => {
+import { getCurrentUser } from '@/lib/actions/auth.action'
+const page = async() => {
+  const user = await getCurrentUser();
   return <>
     <h3>Interview Generation</h3>
-    <Agent userName="You" userId="user1" type="generate"></Agent>
+    <Agent userName={user?.name} userId={user?.id} type="generate"></Agent>
   </>
 }
 
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -26,24 +26,21 @@ export async function signUp(params: SignUpParams) {
             success: true,
             message: 'Account created successfully. Please sign in.'
         }
-    } catch (e: unknown) {
-    console.error('Error creating a user', e);
+    } catch (e: any) {
+        console.error('Error creating a user', e);
 
-    if (e instanceof Error && 'code' in e) {
-        const errorWithCode = e as { code: string };
-        if (errorWithCode.code === 'auth/email-already-exists') {
+        if(e.code === 'auth/email-already-exists') {
             return {
                 success: false,
                 message: 'This email is already in use.'
-            };
+            }
+        }
+
+        return {
+            success: false,
+            message: 'Failed to create an account'
         }
     }
-
-    return {
-        success: false,
-        message: 'Failed to create an account'
-    };
-}
 }
 
 export async function signIn(params: SignInParams) {
@@ -118,4 +115,34 @@ export async function isAuthenticated() {
     const user = await getCurrentUser();
 
     return !!user;
+}
+
+export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
+    const interviews = await db
+        .collection('interviews')
+        .where('userId', '==', userId)
+        .orderBy('createdAt', 'desc')
+        .get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    })) as Interview[];
+}
+
+export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
+    const { userId, limit = 20 } = params;
+
+    const interviews = await db
+        .collection('interviews')
+        .orderBy('createdAt', 'desc')
+        .where('finalized', '==', true)
+        .where('userId', '!=', userId)
+        .limit(limit)
+        .get();
+
+    return interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    })) as Interview[];
 }
